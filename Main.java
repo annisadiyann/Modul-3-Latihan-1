314f0cdb7a2ab0a022b92ba1d1de71c049eb22e4
annisadiyann <140324848+annisadiyann@users.noreply.github.com>
2023-10-30 05:12:42 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+  System.out.println("Masukkan nama anda: ");
+    String nama = scanner.nextLine();
+
+  System.out.println("Hallo, " + nama + " Selamat Datang di GIT!");
+  }
+}
